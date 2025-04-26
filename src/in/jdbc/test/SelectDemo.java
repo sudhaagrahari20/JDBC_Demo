de48b5d91f687e396ad0d61dccd656c8f9c49250
Sudha Agrahari <117359974+sudhaagrahari20@users.noreply.github.com>
2025-04-27 03:18:33 +0530

    Create SelectDemo.java

---
+++ src/in/jdbc/test/SelectDemo.java
@@ -0,0 +1,40 @@
+package in.jdbc.test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class SelectDemo {
+	public static void main(String[] args) throws Exception {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc_db", "root", "SUDhar@!?234");
+		PreparedStatement ps = con.prepareStatement("select * from register");
+		
+		ResultSet rs = ps.executeQuery();
+		
+		while(rs.next()) {
+			String name1 = rs.getString("name");
+			System.out.println("Name : "+name1);
+			
+			String email1 = rs.getString("email");
+			System.out.println("Email : "+email1);
+			
+			String pass1 = rs.getString("password");
+			System.out.println("Password : "+pass1);
+			
+			String gender1 = rs.getString("gender");
+			System.out.println("Gender : "+gender1);
+			
+			String city1 = rs.getString("city");
+			System.out.println("City : "+city1);
+			
+			System.out.println("----------------");
+		}
+		con.close();
+				
+		
+	}
+
+}
